bd6dbf26e5aec8917dae3f9da385f70a5e774d3b
Infinity <64598947+serplay@users.noreply.github.com>
2023-10-24 23:19:53 +0200

    tt

--- spotify_control.py
+++ spotify_control.py
@@ -6,6 +6,6 @@ CLIENT_SECRET = '022524ff98bc40ff907eba3798736d0e'
 SCOPES = 'user-read-playback-state'
 
 token = SpotifyOAuth(client_id=CLIENT_ID,client_secret=CLIENT_SECRET,redirect_uri='http://localhost:8080',scope=SCOPES)
-print(token)
 sp = spotipy.Spotify(auth_manager=token)
 print(sp.devices())
+print(sp.user_playlists(user='serplay'))
