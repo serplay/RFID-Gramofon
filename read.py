06832199c12ed79fbb716a655aab2dfe82f45f0f
serplay <kuba20160101@gmail.com>
2023-10-27 02:37:29 +0200

    json save system

--- read.py
+++ read.py
@@ -3,6 +3,7 @@ from mfrc522 import SimpleMFRC522
 from spotify_control import *
 import asyncio
 import sys
+import json
 
 reader = SimpleMFRC522()
 is_reading = True
@@ -17,14 +18,18 @@ GPIO.setwarnings(False)
 
 #async def main():
     #await asyncio.create_task(read(reader))
-    
+
 
 while True:
     id = reader.read_id_no_block()
     print(id)
     if id != None:
         wejscie = input()
-    if (id not in albumy) and id != None and wejscie == '1':
+    if id != None and wejscie == '1':
         albumy[id] = get_current_album()
     elif id in albumy and id != None and wejscie == '0':
         print(get_current_album, albumy[id])
+    
+
+    with open('save.json','w') as f:
+        json.dump(albumy,f)
