8cbb221bfe6eb117a249d832da1a50ed3144c310
serplay <kuba20160101@gmail.com>
2023-10-27 03:26:12 +0200

    saving albums works

--- read.py
+++ read.py
@@ -1,35 +1,28 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from spotify_control import *
-import asyncio
-import sys
 import json
+GPIO.setwarnings(False)
 
 reader = SimpleMFRC522()
 is_reading = True
 
-id = None
+id = 'None'
 
-GPIO.setwarnings(False)
-
-#def read(reader):
-#        global id
-#            await asyncio.sleep(0.5) # we only read every 0.5 seconds to give other coroutines time to do theri thing
-
-#async def main():
-    #await asyncio.create_task(read(reader))
-
+with open('save.json') as f:
+    albumy = json.load(f)
+albumy_start = dict.copy(albumy)
 
 while True:
-    id = reader.read_id_no_block()
-    print(id)
-    if id != None:
-        wejscie = input()
-    if id != None and wejscie == '1':
-        albumy[id] = get_current_album()
-    elif id in albumy and wejscie == '0':
-        print(get_current_album, albumy[id])
+    id = str(reader.read_id_no_block())
     
-
-    with open('save.json','w') as f:
-        json.dump(albumy,f)
+    if id != 'None':
+        wejscie = input('awaiting input: ')
+    if id != 'None' and wejscie == '1':
+        albumy[id] = get_current_album()
+    elif (id in albumy) and wejscie == '0':
+        print(get_current_album(), albumy[id])
+    
+    if albumy != albumy_start:
+        with open('save.json','w') as f:
+            json.dump(albumy,f)
